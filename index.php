00a282b2be815e9e55a2a8271ebdb5b5b501daee
Nagata Ginga <ginga.nagata@rakuten-sec.co.jp>
2021-08-19 16:03:42 +0900

    CSSを書く

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta chatset="UTF-8" />
+    <title>tech boost</title>
+
+    <!-- コメント -->
+    <style>
+    h1 {
+        color: red;
+    }
+    </style>
+    <!-- ここまで追加 -->
+</head>
+    <body>
+        <h1>Hello world!!!</h1>
+        <p>はじめてのwebサイトへようこそ</p>
+</body>
+</html>
